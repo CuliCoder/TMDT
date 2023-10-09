3601606e24b1a9a643fa954108483d71c35eaab8
trungnc113 <nguyencongtrung13@gmail.com>
2023-10-09 09:48:25 +0700

    up

--- dropdown_topbar.js
+++ dropdown_topbar.js
@@ -2,8 +2,10 @@ let search = document.querySelector(".bx-search");
 let search_place = document.querySelector(".search-place");
 let cart = document.querySelector(".bx-shopping-bag");
 let place_check_cart = document.querySelector(".check-cart");
+let input_search = document.querySelector(".input-search");
 function showdropdown_search() {
   search_place.classList.toggle("show-drop-down");
+  if (search_place.classList.contains("show-drop-down")) input_search.focus();
 }
 function showdropdown_cart() {
   place_check_cart.classList.toggle("show-drop-down");
