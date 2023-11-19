e1fa63cd9eabb6a0d90b5392e155b04f8804dcba
trungnc113 <nguyencongtrung13@gmail.com>
2023-11-19 22:06:12 +0700

    up

--- dropdown_topbar.js
+++
@@ -1,6 +0,0 @@
-let icon_search = document.querySelector(".header .search ");
-let search_box = document.querySelector(".search-bg");
-icon_search.addEventListener("click", () => {
-  search_box.classList.toggle("hide");
-  document.querySelector(".search-bg input").focus();
-});
