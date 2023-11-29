54f89c29d6d87dd3361759518accfbb2eb8baea8
trungnc113 <nguyencongtrung13@gmail.com>
2023-11-29 19:57:33 +0700

    end demo

--- js/statistics_page.js
+++ js/statistics_page.js
@@ -70,3 +70,70 @@ function price_format(price) {
   tmp = tmp.slice(0);
   return tmp + price_str + "₫";
 }
+//lọc theo ngày
+function filter_date(date) {
+  let from = document.querySelector("#date-begin").valueAsDate;
+  let to = document.querySelector("#date-end").valueAsDate;
+  if (from <= new Date(date) && new Date(date) <= to) return true;
+  return false;
+}
+function filter(brand) {
+  let quantity = 0;
+  let total_price = 0;
+  let users = JSON.parse(localStorage.getItem("users"));
+  for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < users[i].order.length; j++) {
+      if (
+        users[i].order[j].status == "Đơn hàng đã được xác nhận" &&
+        filter_date(users[i].order[j].date_purchase)
+      ) {
+        //chỉ tính những đơn hàng đã được xác nhận
+        for (let v = 0; v < users[i].order[j].products.length; v++) {
+          if (users[i].order[j].products[v].brand == brand) {
+            quantity += users[i].order[j].products[v].quantity;
+            total_price += users[i].order[j].products[v].total_price;
+          }
+        }
+      }
+    }
+  }
+  return [quantity, total_price];
+}
+document.querySelector(".search-box button").addEventListener("click", () => {
+  document.querySelector(
+    ".list-brands .brands"
+  ).innerHTML = `<li class="top-list">
+  <ul>
+    <li>STT</li>
+    <li>Hãng</li>
+    <li>Số lượng bán ra</li>
+    <li>Doanh thu</li>
+  </ul>
+</li>`;
+  let array_brand = ["APPLE", "SAMSUNG", "XIAOMI", "OPPO", "REALME"];
+  let total = [0, 0];
+  for (let i = 0; i < array_brand.length; i++) {
+    let Filter = filter(array_brand[i]); //lấy số lượng và tổng tiền từng hãng
+    total[0] += Filter[0]; //tổng sl tất cả hãng
+    total[1] += Filter[1]; //tổng tiền tất cả hãng
+    document.querySelector(
+      ".list-brands .brands"
+    ).innerHTML += `<li class="brand">
+    <ul>
+      <li>${i + 1}</li>
+      <li>${array_brand[i]}</li>
+      <li>${Filter[0]}</li>
+      <li>${price_format("" + Filter[1])}</li>
+    </ul>
+  </li>`;
+  }
+  document.querySelector(
+    ".list-brands .brands"
+  ).innerHTML += `<li class="brand">
+  <ul>
+    <li>Tổng</li>
+    <li>${total[0]}</li>
+    <li>${price_format("" + total[1])}</li>
+  </ul>
+</li>`;
+});
